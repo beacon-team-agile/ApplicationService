cbb38c6572512745616b178a50d5beb2c879bd26
Yang Bo <VictorBoYang@gmail.com>
2022-08-15 10:36:26 -0400

    ApplicationService init

---
+++ ApplicationService/src/main/java/com/teamagile/applicationservice/ApplicationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.teamagile.applicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationServiceApplication.class, args);
+	}
+
+}
---
+++ ApplicationService/src/main/java/com/teamagile/applicationservice/entity/ApplicationWorkFlow.java
@@ -0,0 +1,35 @@
+package com.teamagile.applicationservice.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "ApplicationWorkFlow")
+public class ApplicationWorkFlow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "employee_id")
+    private String employee_id;
+
+    @Column(name = "create_date")
+    private String create_date;
+
+    @Column(name = "last_modification_date")
+    private String last_modification_date;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @Column(name = "comment")
+    private String comment;
+}
